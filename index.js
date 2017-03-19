45f0e37c25011a22cf6c1caf76944fc887f1ce63
Charlie Friend <charles.d.friend@gmail.com>
2017-03-19 01:16:25 -0700

    Switch http port to 80

--- index.js
+++ index.js
@@ -104,6 +104,6 @@ app.get('/removeLeaveWorkAlerts', function(req, res) {
 	});
 });
 
-app.listen(8080, function() {
-    console.log("Listening on port 8080");
+app.listen(80, function() {
+    console.log("Listening on port 80");
 });
