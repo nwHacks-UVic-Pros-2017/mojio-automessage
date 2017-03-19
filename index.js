4d0d0d6d5ed51052ee9f87e2df3f1a6691c9d418
Shae Brown <shaeb@uvic.ca>
2017-03-18 19:22:07 -0700

    Working calls to mojio

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 "use strict";
 
 var express = require('express');
+var MojioUser = require('./lib/MojioUser.js');
 var app = express();
 
 app.get('/', function(req, res) {
+	var mojio = new MojioUser();
+    mojio.authorize('aj.podeziel@gmail.com', 'ZTxNPJvx7@vGw0', function(success) {
+    	if (success) {
+    		console.log("Authenticated");
+    		mojio.suscribe_all_user_vehicles(function(success) {
+		    	if (success) {
+		    		console.log("Success registering vehicles");
+		    	} else {
+			    	console.log("Error with vehicles");
+			        console.log(err);
+		    	}
+    		});
+    	} else {
+			console.log("NOT Authenticated");
+    	}
+    });
     res.send("I'm an app!!!!!!!!!!!");
+});
+
+app.post('/:vehicle_id/ignition_on', function(req, res) {
+	console.log("Car turned on");
+    console.log(req);
 });
 
 app.listen(8080, function() {
