98256ae9a50795abe4db363b53af292906f06c32
Shae Brown <shaeb@uvic.ca>
2017-03-19 02:32:39 -0700

    Lil fix

--- GoogleHandler.js
+++ GoogleHandler.js
@@ -4,7 +4,7 @@ class GoogleHandler {
 
 	constructor() {
 		this.apiKey = process.env.GOOGLE_API_KEY;
-		console.log(apiKey);
+		console.log(this.apiKey);
 		this.url = "https://maps.googleapis.com/maps/api/directions/xml?"
 		this.distance_threadhold = 500; //meters
 	}
